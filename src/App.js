e08f91a7844f8be68e23635e9b2eac078a5d3aa8
SumithBandela <sumithbandela@gmail.com>
2025-04-25 13:25:03 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
           <Container>
             {/* Logo Section */}
             <Navbar.Brand as={Link} to="/home" className="nav-link">
-            <span className='logo'>{cookies.adminUser}</span>
+            <span className='logo'>{cookies.adminUser} Photography</span>
             </Navbar.Brand>
 
             {/* Navbar Toggle for Mobile */}
