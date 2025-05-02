c33e096ad58921c19f56b58489f1508427f9d3bc
SumithBandela <sumithbandela@gmail.com>
2025-05-02 14:15:06 +0530

    commit

--- src/admin-contact.jsx
+++ src/admin-contact.jsx
@@ -15,8 +15,7 @@ export function ContactAdmin() {
         <p className="contact-admin-message">
           Your account does not have an active subscription. Please contact admin to activate your access.
           If you believe this is an error or need assistance, feel free to reach out to us via email at 
-          <a href="contactus@rashmiphotography.com" className="contact-admin-email ms-2">contactus@rashmiphotography.com</a> or use the 
-          contact form below. We will assist you promptly to ensure your account gets back online.
+          <a href="contactus@rashmiphotography.com" className="contact-admin-email ms-2">contactus@rashmiphotography.com</a>. We will assist you promptly to ensure your account gets back online.
         </p>
         <button className="btn-complete-profile" onClick={handleLogout}>
           logout
